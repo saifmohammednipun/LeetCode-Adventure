89be07356e721846236a04c0fef057b16842b13e
saifmohammednipun <saifmohammednipun@gmail.com>
2023-07-04 02:31:56 +0600

    Implement Stack using Queues - LeetCode

---
+++ 232_Solution.cpp
@@ -0,0 +1,57 @@
+class MyStack
+{
+public:
+    queue<int> q;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+    }
+
+    int pop()
+    {
+        queue<int> newQ;
+        int last;
+        while (!q.empty())
+        {
+            int k = q.front();
+            q.pop();
+            if (q.empty())
+            {
+                // last element
+                last = k;
+                break;
+            }
+            newQ.push(k);
+        }
+        q = newQ;
+        return last;
+    }
+
+    int top()
+    {
+        queue<int> newQ;
+        int last;
+        while (!q.empty())
+        {
+            int k = q.front();
+            q.pop();
+            if (q.empty())
+            {
+                // last element
+                last = k;
+            }
+            newQ.push(k);
+        }
+        q = newQ;
+        return last;
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
