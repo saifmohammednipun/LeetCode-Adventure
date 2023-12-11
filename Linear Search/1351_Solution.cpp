e126b66342743e723a13ee1b1bd668a1565e95bd
saifmohammednipun <saifmohammednipun@gmail.com>
2023-12-12 02:52:37 +0600

    Runtime: 10ms(82.14%), Memory: 10.7MB(43.34%)

--- Linear Search/1351_Solution.cpp
+++ Linear Search/1351_Solution.cpp
@@ -3,7 +3,6 @@ public:
     int countNegatives(vector<vector<int>>& grid)
     {
         int count = 0;
-
         for(int i=0; i<grid.size(); i++)
         {
             for(int j=0; j<grid[i].size(); j++)
@@ -15,7 +14,6 @@ public:
             }
        
         }
-        
         return count;
     }
 };
